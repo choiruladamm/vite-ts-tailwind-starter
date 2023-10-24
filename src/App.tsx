31dbe6e6f104d355e7cc11c220487fe2df96c52f
Choirul Adam <109071310+choiruladamm@users.noreply.github.com>
2023-10-24 12:44:51 +0700

    feat: add introduction page (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
 import { FC } from 'react';
-import { Button } from './components/ui/button';
+import { Icons } from './components/Icons';
+import { cn } from './lib/utils';
+import { buttonVariants } from './components/ui/button';
 
 interface AppProps {}
 
-const App: FC<AppProps> = () => {
+const App: FC<AppProps> = ({}) => {
 	return (
-		<div className='grid min-h-screen place-items-center'>
-			<Button>vite-ts-tailwind-starter</Button>
+		<div className='layout relative flex min-h-screen flex-col items-center justify-center gap-6 text-center'>
+			<div className='flex gap-4'>
+				<Icons.bolt className='h-16 w-16' />
+				<Icons.react className='h-16 w-16 text-sky-500' />
+			</div>
+			<div className='flex flex-col items-center gap-4 md:gap-2'>
+				<h1 className='text-3xl font-bold'>
+					React.js + Tailwind CSS + TypeScript Starter
+				</h1>
+				<p className='max-w-3xl text-sm text-gray-600 md:text-base'>
+					A starter for React.js, Tailwind CSS, and TypeScript with{' '}
+					<span className='font-semibold text-slate-900'>shadcnui,</span> {' '}
+					Beautifully designed components built with Radix UI and Tailwind CSS.
+				</p>
+				<a
+					href='https://github.com/choiruladamm/vite-ts-tailwind-starter'
+					target='_blank'
+					className={cn(
+						buttonVariants({ variant: 'link' }),
+						'hover:text-sky-600',
+					)}
+				>
+					See the repository
+				</a>
+			</div>
+			<footer className='absolute bottom-2 flex gap-1 text-gray-600'>
+				© 2023 By
+				<a
+					href='https://choiruladamm.vercel.app/'
+					target='_blank'
+					className='cursor-pointer font-semibold hover:text-sky-600 hover:underline'
+				>
+					Choirul Adamm
+				</a>
+			</footer>
 		</div>
 	);
 };
