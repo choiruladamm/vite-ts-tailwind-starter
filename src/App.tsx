bb3879a2b6396c853f1fdef1fa76e757243cc993
choiruladamm <choiruladamm@gmail.com>
2023-10-24 08:55:38 +0700

    feat: add automatic class sorting prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { FC } from "react";
-import { Button } from "./components/ui/button";
+import { FC } from 'react';
+import { Button } from './components/ui/button';
 
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
-  return (
-    <div className="grid place-items-center min-h-screen">
-      <Button>vite-ts-tailwind-starter</Button>
-    </div>
-  );
+	return (
+		<div className='grid min-h-screen place-items-center px-10 py-4'>
+			<Button>vite-ts-tailwind-starter</Button>
+		</div>
+	);
 };
 
 export default App;
