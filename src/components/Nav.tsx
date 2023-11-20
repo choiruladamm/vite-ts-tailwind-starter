a92f7b4be5dadfc306678ea88ddf2da7fe036e9a
choiruladamm <choiruladamm@gmail.com>
2023-11-20 14:04:39 +0700

    refactor: change code link to map object

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,15 +6,19 @@ interface NavProps {}
 
 const Nav: FC<NavProps> = ({}) => {
   return (
-    <header className='absolute top-0 z-50 w-full text-slate-900 py-3 shadow-sm'>
-      <div className='layout flex justify-between items-center '>
+    <header className='absolute top-0 z-50 w-full py-3 text-slate-900 shadow-sm'>
+      <div className='layout flex items-center justify-between '>
         <div className='flex gap-4'>
-          <Link to='/'>Home</Link>
-          <Link to='/projects'>Projects</Link>
-          <Link to='/blog'>Blog</Link>
-          <Link to='/about'>About</Link>
+          {links.map((link) => (
+            <Link key={link.label} to={link.href}>
+              {link.label}
+            </Link>
+          ))}
         </div>
-        <Link to='https://github.com/choiruladamm/vite-ts-tailwind-starter' target='_blank'>
+        <Link
+          to='https://github.com/choiruladamm/vite-ts-tailwind-starter'
+          target='_blank'
+        >
           <Icons.gitHub className='h-5 w-5' />
         </Link>
       </div>
@@ -23,3 +27,22 @@ const Nav: FC<NavProps> = ({}) => {
 };
 
 export default Nav;
+
+const links = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+  {
+    href: '/projects',
+    label: 'Projects',
+  },
+  {
+    href: '/blog',
+    label: 'Blog',
+  },
+  {
+    href: '/about',
+    label: 'About',
+  },
+];
