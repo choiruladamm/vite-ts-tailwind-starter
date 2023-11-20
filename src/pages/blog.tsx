9dabd3552b6b97e45becdcf631e67329040b1b31
Choirul Adam <109071310+choiruladamm@users.noreply.github.com>
2023-11-20 19:54:36 +0700

    feat: add pages layout  (#7)
    * feat: add navbar & pages layout
    
    * refactor: change code link to map object
    
    * feat: add style active current path navlinks

---
+++ src/pages/blog.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface BlogPageProps {}
+
+const BlogPage: FC<BlogPageProps> = ({}) => {
+  return <div>Blog Page</div>;
+};
+
+export default BlogPage;
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as HomePage } from './home';
+export { default as ProjectsPage } from './projects';
+export { default as AboutPage } from './about';
+export { default as BlogPage } from './blog';
+export { default as RootLayout } from './root-layout';
